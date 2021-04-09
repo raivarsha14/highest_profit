00234e1087a1ef954e47bdacc19961024b4e558b
Varsha Rai <rai.varsha.14@gmail.com>
2021-04-08 22:51:28 -0400

    Adding additional comments to the python file

--- highest_profit.py
+++ highest_profit.py
@@ -1,29 +1,33 @@
-# Importing pandas library needed to solve the challange
+# Importing pandas library needed to solve the challenge
 import pandas as pd
+# Importing OS library to work with directory paths
 import os
 
-# To read the data from csv file I am using pandas and putting the data into dataframe named as 'df'
+# To read the data from RAW csv file, I am using pandas and putting the data into dataframe named as 'df'
 df = pd.read_csv("https://gist.githubusercontent.com/bobbae/b4eec5b5cb0263e7e3e63a6806d045f2/raw/279b794a834a62dc108fc843a72c94c49361b501/data.csv")
 
-# Printing first answer using shape function. Shape returns answer in tuple (no. of rows, no. of columns), since only rows are requested therefore accessing 0th element 
+# Printing first answer using shape method of the dataframe. Shape returns answer in a tuple (no. of rows, no. of columns).
+# Since only rows are requested, I am  accessing 0th element to retrieve the number of rows in the dataframe
 print(f"Answer 1: There are {df.shape[0]} number of rows")
 
-#For the second answer, I was supposed to eleminate the rows with non-numeric values
-#First I will convert the 'profit' column into numeric values, where there is not numeric value the error will 
-#reflect and that will be coerced and that will result into 'null' value
+# For the second answer, I am supposed to eleminate the rows with non-numeric values.
+# First I will convert the 'profit' column into numeric type. When the column contains non-numeric value, there would usually be an error. 
+# In case of such an error, I am forcing pandas to place NaN (a valid numeric type) in the cell using the 'coerce' option.
 df['Profit (in millions)'] = pd.to_numeric(df['Profit (in millions)'], errors='coerce')
 
-#By using dropna function I can remove NaN values placesd in above code from 'profit' column
+# By using dropna method of the dataframe, I can remove NaN values placesd in above code from 'profit' column. Thus removing non-numeric profit rows.
 df = df.dropna(axis=0, subset=['Profit (in millions)'])
 
-# To write the JSON file with only valid numeric profit
+# To write the JSON file with all the data where profit column contains only numeric values
 df.to_json("data2.json")
 
-#Printing second answer using shape function displaying numeric rows only
-print(f"Answer 2: There are {df.shape[0]} number of rows are left after removing all the rows with invalid non-numeric profit column data and the dataset has been saved as JSON file to {os.path.join(os.getcwd(), 'data2.json')}")
+# Printing second answer to screen using shape method as this now gets us the number of rows with numeric profit values.
+# Also, printing the path of created JSON file which contains this data.
+print(f"Answer 2: There are {df.shape[0]} number of rows are left after removing all the rows with \
+	invalid non-numeric profit column data and the dataset has been saved as JSON file to {os.path.join(os.getcwd(), 'data2.json')}")
 
 
-# To order the data into decsending order based on 'profit' column
+# To sort the data into decsending order based on 'profit' column
 df = df.sort_values('Profit (in millions)', ascending=False)
 
 print("Answer 3: Top 20 rows after sorting by Profit in descending order:")
